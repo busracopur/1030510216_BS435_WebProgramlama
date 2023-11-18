4886232ea384e8452e89eb7f5f6ab0c8fffa2344
Büşra Çopur <busra.copur2801@gmail.com>
2023-11-18 18:22:14 +0300

    create(game-structure): created The app file has been updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [targetNumber, setTargetNumber] = useState(generateRandomNumber());
+  const [userGuess, setUserGuess] = useState('');
+  const [message, setMessage] = useState('');
+
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+  }
+
+  function handleInputChange(event) {
+    setUserGuess(event.target.value);
+  }
+
+  function handleGuess() {
+    const guess = parseInt(userGuess, 10);
+
+    if () {
+      setMessage('Lütfen geçerli bir sayı girin.');
+      return;
+    }
+
+    if () {
+      setMessage('Tebrikler! Doğru tahmin!');
+    } else {
+
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React happy
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Sayı Tahmin Oyunu</h1>
+        <p>{message}</p>
+        <input
+            placeholder="Bir sayı girin"
+        />
+        <button onClick={handleGuess}>Tahmin Et</button>
+      </div>
   );
 }
 
 export default App;
+
